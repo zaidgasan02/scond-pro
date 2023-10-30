3a5b5bca4578a317def3dde5745bea85453c49b1
zaidgasan02 <149387550+zaidgasan02@users.noreply.github.com>
2023-10-30 16:45:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var travel_place;
+<script>
+
+switch(travel_place){
+	case "1":
+	travel_place="paris";
+	break;
+	case "2":
+	travel_place="";
+	break;
+	case "3":
+	travel_place="";
+	break;
+	case "4":
+	travel_place="";
+	break;
+	case "5":
+	travel_place="";
+	break;
+	case "6":
+	travel_place="";
+	break;
+	case "7":
+	travel_place="";
+	break;
+	case "8":
+	travel_place="";
+	break;
+	case "9":
+	travel_place="";
+	break;
+	case "10":
+	travel_place="";
+	break;
+	default:
+     travel_place:false;
+     break;	 
+	
+	
+	
+}
